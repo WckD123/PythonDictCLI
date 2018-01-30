1cb75fa3c618dbdb08d52f64ae3dea02b05a82d8
WckD123 <mkmehulkaushik@gmail.com>
2018-01-30 20:45:37 +0530

    Added close words option using difflib

--- app1.py
+++ app1.py
@@ -1,5 +1,5 @@
 import json
-import string
+from difflib import *
 
 data = json.load(open("data.json"))
 
@@ -7,6 +7,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return "\n".join(data[w])
+    elif len(get_close_matches(w,data.keys()))>0:
+        return "Did you mean: " + " or ".join(get_close_matches(w,data.keys(),cutoff=0.77))
     else:
         return "Word not found. Please recheck!!"
 
