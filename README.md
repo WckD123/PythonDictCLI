e1658593e5e5cd5691ce187f73441bd660709c55
WckD123 <mkmehulkaushik@gmail.com>
2018-01-30 21:52:09 +0530

    Fixed exception cases and finalized the Yes/No function.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PythonDictCLI
+# Python Dictionary CLI
 
 This is my first Python Project.
 
@@ -7,6 +7,7 @@ I have created a Python Command Line Interface based Dictionary.
 This dictionary has the following basic features:
 
 1) Enter the word -> You'll get its meaning
-2) Upon entering a wrong word, the program will give you other words which are close to the original words
+2) Upon entering a wrong word, the program will give you other words which are close to the original words.
+3) A final Yes/No prompt will help you get the meaning of the suggested word or run the program again.
 
 Note: I tried implementing this on my own but realized that the get_close_matches function of the "difflib" was the best option to do so. Hence, that has been used.
--- app1.py
+++ app1.py
@@ -5,22 +5,33 @@ data = json.load(open("data.json"))
 
 def translate(w):
     w = w.lower()
+    cap = w.capitalize()
+    full_caps = w.upper()
     if w in data:
         print("\n".join(data[w]))
+    elif cap in data:
+        print("\n".join(data[cap]))
+    elif full_caps in data:
+        print("\n".join(data[full_caps]))
     elif len(get_close_matches(w,data.keys()))>0:
         new_word = get_close_matches(w,data.keys())[0]
         print("Did you mean: " + new_word)
-        check = input("Enter y or n: ")
-        check = check.lower()
-        if check == "yes" or check == "y":
-            print("\n".join(data[new_word]))
-        else:
-            meaning()
+        check(new_word)
     else:
         print("Word not found. Please recheck!!")
 
 def meaning():
     word = input("Please Enter The Word: \n")
     translate(word)
+
+def check(new_word):
+    check = input("Enter y or n: ")
+    check = check.lower()
+    if check == "yes" or check == "y":
+        print("\n".join(data[new_word]))
+    elif check == "no" or check == "n":
+        meaning()
+    else:
+        print("Sorry we didn't recognize that input.")
 
 meaning()
